a3bed435f010c6bb821e4100106d2e6f68217a7f
Damian Banach <damian070799@wp.pl>
2024-05-10 10:52:15 +0200

    fix

--- filmy/urls.py
+++ filmy/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('wszystkie/', wszystkie),
     path('szczegoly/<int:film_id>/', szczegoly),
     path('nowy/', nowy),
-    path('edycja/<int:film_id>', edycja),
+    path('edycja/<int:film_id>/', edycja),
     path('usun/<int:film_id>/', usun)
 ]
