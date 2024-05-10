a129791214a82cdcba41f39473a04d58784eac31
Damian Banach <damian070799@wp.pl>
2024-05-10 14:09:40 +0200

    updated settings.py

--- filmweb/settings.py
+++ filmweb/settings.py
@@ -103,3 +103,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 1
 }
+
+GRAPHENE = {
+    "SCHEMA": "filmy.schema.schema"
+}
