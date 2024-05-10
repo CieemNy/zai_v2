cf2d38875afc90ab4365104078073ff46f0945a5
Damian Banach <damian070799@wp.pl>
2024-05-10 11:17:34 +0200

    updated urls.py

--- filmy/urls.py
+++ filmy/urls.py
@@ -1,10 +1,6 @@
+from .views import FilmList
 from django.urls import path
-from .views import *
 
 urlpatterns = [
-    path('wszystkie/', wszystkie),
-    path('szczegoly/<int:film_id>/', szczegoly),
-    path('nowy/', nowy),
-    path('edycja/<int:film_id>/', edycja),
-    path('usun/<int:film_id>/', usun)
+    path('filmlist/', FilmList.as_view(), name='FilmList')
 ]
