936fda9d92adc51661148f080b75776f527c4723
Damian Banach <damian070799@wp.pl>
2024-05-10 10:26:11 +0200

    register model in admin.py

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Film
 
-# Register your models here.
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tytul', 'rok', 'opis', 'premiera', 'imdb_pkts']
+
+
+admin.site.register(Film, FilmAdmin)
