417c63589baabb9d9422f7e3a636ad6299729745
Damian Banach <damian070799@wp.pl>
2024-05-10 11:46:15 +0200

    editied films views

--- filmy/views.py
+++ filmy/views.py
@@ -2,19 +2,33 @@ from django.contrib.auth.models import User
 from .models import *
 from .serializers import *
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .permissions import *
 
 
 class FilmCreateList(generics.ListCreateAPIView):
-    queryset = Film.objects.all().order_by('-rok', 'tytul')
+    # queryset = Film.objects.all().order_by('-rok','tytul')
     serializer_class = FilmModelSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        queryset = Film.objects.all().order_by('-rok','tytul')
+        tytul = self.request.query_params.get('tytul')
+        id = self.request.query_params.get('id')
+        if tytul is not None:
+            queryset = queryset.filter(tytul__startswith=tytul)
+        if id is not None:
+            queryset = queryset.filter(id__exact=id)
+        return queryset
 
 
 class FilmRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmModelSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class ExtraInfoCreateList(generics.ListCreateAPIView):
