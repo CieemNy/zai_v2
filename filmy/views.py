8c5d35023d848a5473b23eaa978ed1efc4741588
Damian Banach <damian070799@wp.pl>
2024-05-10 12:13:15 +0200

    added api_root to views

--- filmy/views.py
+++ filmy/views.py
@@ -1,6 +1,9 @@
 from .models import Film
 from .serializers import *
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class FilmList(generics.ListAPIView):
@@ -26,3 +29,13 @@ class UserList(generics.ListAPIView):
 class UserCreateList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'Użytkownicy': reverse('ListaUzytkownikow', request=request, format=format),
+        'Wszystkie filmy': reverse('ListaFilmow', request=request, format=format),
+        'Informacje dodatkowe': reverse('InformacjeDodatkowe', request=request, format=format),
+        'Wszystkie oceny': reverse('Recenzje', request=request, format=format),
+        'Wszyscy aktorzy': reverse('Aktorzy', request=request, format=format),
+    })
