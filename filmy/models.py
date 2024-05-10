a498bc3df9de9b6042f3d7f5b5f3dcb3afc4480b
Damian Banach <damian070799@wp.pl>
2024-05-10 10:57:39 +0200

    created models: ExtraInfo, Ocena and Aktor

--- filmy/models.py
+++ filmy/models.py
@@ -10,3 +10,26 @@ class Film(models.Model):
 
     def __str__(self):
         return "{} ({})".format(self.tytul, str(self.rok))
+
+
+class ExtraInfo(models.Model):
+    GATUNEK = {
+        (0, 'Inne'),
+        (1, 'Horror'),
+        (2, 'Komedia'),
+        (3, 'Sci-fi'),
+        (4, 'Dramat')
+    }
+    czas_trwania = models.PositiveSmallIntegerField(null=True, blank=True)
+    gatunek = models.PositiveSmallIntegerField(choices=GATUNEK, null=True, blank=True)
+    rezyser = models.CharField(max_length=50, blank=True, null=True)
+
+
+class Ocena(models.Model):
+    recenzja = models.TextField(default="", blank=True)
+    gwiazdki = models.PositiveSmallIntegerField(default=5)
+
+
+class Aktor(models.Model):
+    imie = models.CharField(max_length=32)
+    nazwisko = models.CharField(max_length=32)
