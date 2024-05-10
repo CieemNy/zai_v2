9518069b778aa6ae48859ba861592a0706940956
Damian Banach <damian070799@wp.pl>
2024-05-10 11:22:04 +0200

    added UserSerializer to serializers.py

--- filmy/serializers.py
+++ filmy/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Film
+from django.contrib.auth.models import User
 
 
 class FilmModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Film
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
