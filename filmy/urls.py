eae05f284d84eaefa603f2ef395413be9e410c76
Damian Banach <damian070799@wp.pl>
2024-05-10 12:13:47 +0200

    updated urls.py

--- filmy/urls.py
+++ filmy/urls.py
@@ -2,6 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
+    path('', api_root),
     path('filmlist/', FilmList.as_view(), name='FilmList'),
     path('filmretrieve/<int:pk>/', FilmRetrieve.as_view(), name='FilmRetrieve'),
     path('filmcreatelist/', FilmCreateList.as_view(), name='FilmCreateList'),
